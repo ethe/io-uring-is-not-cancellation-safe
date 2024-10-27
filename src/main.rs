7222a5f93adcf8b7401c8c2c656d4967e39fae81
gwo <guozixing@the-basement.io>
2024-10-27 10:52:10 +0000

    use compio

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,50 @@
 use std::thread;
 use std::time::Duration;
 
-use monoio::io::{AsyncReadRentExt, AsyncWriteRentExt};
-use monoio::net::{TcpListener, TcpStream};
-use monoio::time::TimeDriver;
-use monoio::{select, time, IoUringDriver};
+use compio::driver::ProactorBuilder;
+use compio::io::{AsyncReadExt, AsyncWriteExt};
+use compio::net::{TcpListener, TcpStream};
+use compio::time;
+use futures::{pin_mut, FutureExt};
 
-// #[monoio::main(driver = "legacy", enable_timer = true)]
-#[monoio::main(driver = "io_uring", enable_timer = true)]
+#[compio::main]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
     let addr = listener.local_addr().unwrap();
 
     thread::spawn(move || {
-        monoio::start::<TimeDriver<IoUringDriver>, _>(async move {
-            loop {
-                println!("connecting");
-                let mut stream = TcpStream::connect(addr).await.unwrap();
-                println!("connected");
-                let (result, _) = stream.write_all("hello world").await;
-                result.unwrap();
-                let (result, _) = stream.read_exact(vec![0; 11]).await;
-                result.unwrap();
-                time::sleep(Duration::from_millis(1)).await;
-                println!("completed");
-            }
-        });
+        compio::runtime::RuntimeBuilder::new()
+            .with_proactor(ProactorBuilder::new())
+            .build()
+            .unwrap()
+            .block_on(async {
+                let mut counter = 0;
+                loop {
+                    let mut stream = TcpStream::connect(addr).await.unwrap();
+                    println!("connected {}", counter);
+                    let result = stream.write_all("hello world").await;
+                    result.unwrap();
+                    let result = stream.read_exact(vec![0; 11]).await;
+                    result.unwrap();
+                    println!("completed {}", counter);
+                    time::sleep(Duration::from_millis(100)).await;
+                    counter += 1;
+                }
+            });
     });
 
     loop {
-        select! {
-            stream = listener.accept() => {
-                println!("accept");
+        let t1 = listener.accept().fuse();
+        let t2 = time::sleep(Duration::from_millis(1)).fuse();
+        pin_mut!(t1, t2);
+        futures::select! {
+            stream = t1 => {
                 let (mut stream, _) = stream.unwrap();
-                println!("accepted {:?}", stream.peer_addr().unwrap());
-                let (result, buf) = stream.read_exact(vec![0; 11]).await;
-                result.unwrap();
-                println!("{:?}", buf);
-                let (result, _) = stream.write_all(buf).await;
+                let (_, buf) = stream.read_exact(vec![0; 11]).await.unwrap();
+                let result = stream.write_all(buf).await;
                 result.unwrap();
             }
-            _ = time::sleep(Duration::from_millis(1)) => {
-                println!("timeout");
+            _ = t2 => {
                 continue;
             }
         }
